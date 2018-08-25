c81bb4e93a35a0da0346a9ccd6d53bd79395ca88
Stephen Baker <s3342847@student.rmit.edu.au>
2018-08-25 14:03:28 +1000

    Driver class to create basic output. Used cast's, would like to refactor to avoid them...

---
+++ src/sadi/week1/Week1.java
@@ -0,0 +1,41 @@
+package sadi.week1;
+import java.util.ArrayList;
+import java.util.List;
+
+import sadi.week1.interfaces.*;
+import sadi.week1.superheroes.*;
+import sadi.week1.birds.*;
+/**
+ * 
+ * @author wanidala/s3342847
+ * Test harness / driver for model
+ */
+public class Week1 {
+	public void main(String args[])
+	{
+		List<ISuperhero> heroes = new ArrayList<ISuperhero>();
+		List<IFly> flyers = new ArrayList<IFly>();
+		
+		heroes.add(new Aquaman());
+		heroes.add(new Birdman());
+		flyers.add((IFly) heroes.get(1));
+		heroes.add(new Batman());
+		heroes.add(new Superman());
+		flyers.add((IFly) heroes.get(3));
+		heroes.add(new WonderWoman());
+		flyers.add(new Eagle());
+		flyers.add(new Parrot());
+		
+		for(ISuperhero hero: heroes)
+		{
+			hero.saveTheWorld();
+			System.out.println();
+		}
+		
+		for(IFly flyer : flyers)
+		{
+			flyer.fly();
+			System.out.println();
+		}
+	}
+}
